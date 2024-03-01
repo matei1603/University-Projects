83bbb11b25f1f3a7670c28649056f4a6bcf5e6b6
matei1603 <126803809+matei1603@users.noreply.github.com>
2024-03-01 20:05:51 +0200

    Delete Algorithms and Programming/pmmie389_A5/assigment 5 directory

--- Algorithms and Programming/pmmie389_A5/assigment 5/main.py
+++
@@ -1,9 +0,0 @@
-import AirportUI
-import AirportController
-import Airport
-
-airport = Airport()
-ctrl = AirportController(airport)
-ui = AirportUI(ctrl)
-
-ui.start()
--- Algorithms and Programming/pmmie389_A5/assigment 5/run_tests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-def run_all():
-    loader = unittest.TestLoader()
-    suite = loader.discover("./test", pattern = "test_*.py")
-    unittest.TextTestRunner().run(suite)
-
-run_all()
